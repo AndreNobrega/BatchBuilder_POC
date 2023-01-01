274776f1b009f6fac8a4918f4388e7edfb78e2dd
AndreNobrega <andre.ds.nobrega@gmail.com>
2023-01-01 15:27:56 +0100

    Updated SFTP settings

--- UpdateService/src/UpdateService.Core/Entities/Settings/SftpSettings.cs
+++ UpdateService/src/UpdateService.Core/Entities/Settings/SftpSettings.cs
@@ -3,5 +3,9 @@
 	public class SftpSettings : FileTransferSettingsBase
 	{
 		public bool UseSftp { get; set; }
+		public string BaseUrl { get; set; }
+		public string Port { get; set; }
+		public string UserName { get; set; }
+		public string Password { get; set; }
 	}
 }
