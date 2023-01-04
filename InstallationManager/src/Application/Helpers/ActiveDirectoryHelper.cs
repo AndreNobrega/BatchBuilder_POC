4a3cb17e8d2918ff48478643bb8535bb9d60627b
AndreNobrega <andre.ds.nobrega@gmail.com>
2023-01-04 14:50:04 +0100

    Added explanation to pragmas in ActiveDirectoryHelper methods

--- InstallationManager/src/Application/Helpers/ActiveDirectoryHelper.cs
+++ InstallationManager/src/Application/Helpers/ActiveDirectoryHelper.cs
@@ -11,6 +11,7 @@ namespace Application.Helpers
 		/// <returns>True if the user exists, false otherwise.</returns>
 		public static bool ADUserExists(string userName)
 		{
+			// Disable warning: method only works on Windows
 			#pragma warning disable CA1416 // Validate platform compatibility
 			using var context = new PrincipalContext(ContextType.Domain);
 			var adUser = UserPrincipal.FindByIdentity(context, userName);
@@ -26,6 +27,7 @@ namespace Application.Helpers
 		/// <returns>True if the group exists, false otherwise.</returns>
 		public static bool ADGroupExists(string groupName)
 		{
+			// Disable warning: method only works on Windows
 			#pragma warning disable CA1416 // Validate platform compatibility
 			using var context = new PrincipalContext(ContextType.Domain);
 			var adGroup = GroupPrincipal.FindByIdentity(context, groupName);
