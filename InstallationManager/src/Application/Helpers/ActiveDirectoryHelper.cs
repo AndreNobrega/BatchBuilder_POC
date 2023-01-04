591580cbe11eedc212bb84d1e2d9969151275c2e
AndreNobrega <andre.ds.nobrega@gmail.com>
2023-01-04 14:48:47 +0100

    Implemented ActiveDirectoryHelper methods

--- InstallationManager/src/Application/Helpers/ActiveDirectoryHelper.cs
+++ InstallationManager/src/Application/Helpers/ActiveDirectoryHelper.cs
@@ -1,26 +1,37 @@
-﻿using System;
-using System.DirectoryServices.AccountManagement;
-using System.Collections.Generic;
-using System.Reflection.PortableExecutable;
-using System.Text;
+﻿using System.DirectoryServices.AccountManagement;
 
 namespace Application.Helpers
 {
 	public static class ActiveDirectoryHelper
 	{
-		public static bool ADUserExists(string user)
+		/// <summary>
+		/// Checks if a user exists in the Active Directory.
+		/// </summary>
+		/// <param name="userName"></param>
+		/// <returns>True if the user exists, false otherwise.</returns>
+		public static bool ADUserExists(string userName)
 		{
-			throw new NotImplementedException();
+			#pragma warning disable CA1416 // Validate platform compatibility
+			using var context = new PrincipalContext(ContextType.Domain);
+			var adUser = UserPrincipal.FindByIdentity(context, userName);
 
-			//using (var context = new PrinciPalContext())
-			//{
-			//	UserPrincipal principal = new UserPrincipal(context);
-			//}
+			return adUser != null;
+			#pragma warning restore CA1416 // Validate platform compatibility
 		}
 
-		public static bool ADGroupExists(string group)
+		/// <summary>
+		/// Checks if a group exists in the Active Directory.
+		/// </summary>
+		/// <param name="groupName"></param>
+		/// <returns>True if the group exists, false otherwise.</returns>
+		public static bool ADGroupExists(string groupName)
 		{
-			throw new NotImplementedException();
+			#pragma warning disable CA1416 // Validate platform compatibility
+			using var context = new PrincipalContext(ContextType.Domain);
+			var adGroup = GroupPrincipal.FindByIdentity(context, groupName);
+
+			return adGroup != null;
+			#pragma warning restore CA1416 // Validate platform compatibility
 		}
 	}
 }
