c9e4a601d36d9410917f09ed0be6788195b8a2b6
AndreNobrega <andre.ds.nobrega@gmail.com>
2023-01-04 15:41:13 +0100

    Added dependency injection to InstallationManager

--- InstallationManager/src/InstallationManager.Presentation.WPF/App.xaml.cs
+++ InstallationManager/src/InstallationManager.Presentation.WPF/App.xaml.cs
@@ -1,17 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using Application;
+using Infrastructure;
+using Microsoft.Extensions.Hosting;
 
 namespace InstallationManager.Presentation.WPF
 {
 	/// <summary>
 	/// Interaction logic for App.xaml
 	/// </summary>
-	public partial class App : Application
+	public partial class App : System.Windows.Application
 	{
+		public App()
+		{
+			CreateHostBuilder(null).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[]? args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureServices((hostContext, services) =>
+				{
+					services.AddApplicationServices();
+					services.AddInfrastructureServices();
+				});
 	}
 }
